084aac8b732d162775cceaed3ab55d0508056669
SajMaxx <sajan.mathews75@gmail.com>
2020-10-25 15:14:14 -0400

    Scaffolding Example using ASP.Net MVC Application

---
+++ 2_WebProg/ASPNetClassic/ScaffoldApp/Models/WorkerBee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ScaffoldApp.Models
+{
+    public class WorkerBee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime JoinDate { get; set; }
+        public int Age { get; set; }
+        public int ZipCode { get; set; }
+
+    }
+
+    public class WorkerBeeDBContext : DbContext
+    {
+        public DbSet<WorkerBee> WorkerBees { get; set;}
+    }
+}
