f49ef4eb1b86ff4c6e7d4597c8fb6c2370bce9bc
sajmaxx <sajan.mathews75@gmail.com>
2022-03-26 16:06:20 -0400

    threads and lambda

--- linuxCPP/ThreadTry1.cpp
+++ linuxCPP/ThreadTry1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<thread>
+#include<vector>
 
 using namespace std;
 
@@ -13,6 +14,26 @@ void threadWorkSM()
 
 int main()
 {    
+
+    unsigned int numberofThreads = thread::hardware_concurrency();
+
+    cout << " This Computer has " << numberofThreads << " concurrent threads available " << endl;
+
+    vector<int> lablids;
+
+    lablids.push_back(33);
+    lablids.push_back(44);
+    lablids.push_back(77);
+
+    //using inser with iterator into a vector!
+
+    lablids.insert(begin(lablids)+2, 55);
+
+    for(int i =0; i < lablids.size(); i++)
+    {
+        cout << lablids[i] << endl;
+    }
+
     thread smTh(threadWorkSM);
 
 
