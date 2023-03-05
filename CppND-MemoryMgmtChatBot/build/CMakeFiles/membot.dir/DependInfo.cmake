8157aa90a24c65c39b6c98219783ef9693f40720
sajmaxx <sajan.mathews75@gmail.com>
2023-03-04 20:10:41 -0500

    Added CLion Projects March 2023 and also units

---
+++ CppND-MemoryMgmtChatBot/build/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/src/chatbot.cpp" "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/build/CMakeFiles/membot.dir/src/chatbot.cpp.o"
+  "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/src/chatgui.cpp" "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/build/CMakeFiles/membot.dir/src/chatgui.cpp.o"
+  "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/src/chatlogic.cpp" "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/build/CMakeFiles/membot.dir/src/chatlogic.cpp.o"
+  "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/src/graphedge.cpp" "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/build/CMakeFiles/membot.dir/src/graphedge.cpp.o"
+  "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/src/graphnode.cpp" "/home/simonm/repos/CodeShip/CppND-MemoryMgmtChatBot/build/CMakeFiles/membot.dir/src/graphnode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WXUSINGDLL"
+  "_FILE_OFFSET_BITS=64"
+  "__WXGTK__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../C:/wxWidgets-3.1.5/include"
+  "/usr/lib/x86_64-linux-gnu/wx/include/gtk3-unicode-3.0"
+  "/usr/include/wx-3.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
