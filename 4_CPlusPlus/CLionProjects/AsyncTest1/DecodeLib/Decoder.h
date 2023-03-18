80ac0bad69a035204e4e485cc0984dd916bfe514
sajmaxx <sajan.mathews75@gmail.com>
2023-03-18 18:49:28 -0400

    Delete stuff

--- 4_CPlusPlus/CLionProjects/AsyncTest1/DecodeLib/Decoder.h
+++ 4_CPlusPlus/CLionProjects/AsyncTest1/DecodeLib/Decoder.h
@@ -5,4 +5,45 @@
 #ifndef ASYNCTEST1_DECODER_H
 #define ASYNCTEST1_DECODER_H
 
+#include <iostream>
+#include <vector>
+//make a class that does decoding
+
+struct vector3d
+{
+    int x;
+    int y;
+    int z;
+    void print()
+    {
+        std::cout << "Vector Components are:: x: " << x << " y: " << y << " z: " << z << std::endl;
+    }
+};
+
+
+
+class Decoder
+{
+public:
+    Decoder() {}
+    ~Decoder() {}
+
+    vector3d decode(int i, int j, int k)
+    {
+
+        std::cout << "Decoding" << std::endl;
+        //add vector3d
+        m_decoded.push_back(vector3d{i, j, k});
+        return vector3d{i, j, k};
+    }
+
+    int count()
+    {
+        return m_decoded.size();
+    }
+
+private:
+    //vector of vector3d
+    std::vector<vector3d> m_decoded;
+};
 #endif //ASYNCTEST1_DECODER_H
